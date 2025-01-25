5334051274bff4154f145f8d8a0d9a0016968dd1
Himanshu Das <himanshudas11111@gmail.com>
2025-01-26 00:04:35 +0530

    Your commit message here

---
+++ schema.js
@@ -0,0 +1,17 @@
+const Joi = require("joi");
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        
+        // Update this part to validate 'image' as an object with 'filename' and 'url'
+        image: Joi.object({
+            filename: Joi.string().optional(),
+            url: Joi.string().uri().required() // URL should be a string and a valid URI
+        }).optional()  // 'image' is optional in the schema
+    }).required()
+});
---
+++ utils/validateListing.js
@@ -0,0 +1,18 @@
+const { listingSchema } = require("/js/schema.js");  // Assuming the schema is in "schema.js"
+const ExpressError = require("./utils/ExpressError.js");  // Custom error handler
+
+const validateListing = (req, res, next) => {
+    // Joi validation
+    let { error } = listingSchema.validate(req.body);  // Ensure we validate the top-level 'listing'
+    
+    if (error) {
+        // Combine error messages and throw an ExpressError with status code 400 (Bad Request)
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        return next(new ExpressError(400, errMsg));
+    }
+    
+    // Proceed to next middleware or route handler if validation is successful
+    next();
+};
+
+module.exports = validateListing;
